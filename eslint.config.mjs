d2b629dbd674fcbced70bbf4525e5b621d8018ee
yunojang <hanganda23@naver.com>
2025-01-09 18:48:48 +0900

    chore: set config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["off"],
+      "@typescript-eslint/no-empty-object-type": "warn",
+    },
+  }),
 ];
 
 export default eslintConfig;
